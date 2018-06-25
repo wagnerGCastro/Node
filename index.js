233ed467b98d6722cfadaf4158cbfdec8b210031
Wagner <wagner_gcastro@hotmail.com>
2018-06-25 07:40:36 -0300

    nvos arquivos

---
+++ index.js
@@ -0,0 +1,12 @@
+﻿"use strict";
+var http = require('http');
+
+var server =  http.createServer( function(req, res) {
+	res.end('<html><body>Portal de noticias </body></html>');
+});
+
+// 1 - Criação de Servidor/ Execução
+server.listen(3000);
+console.log('Servidor rodando Express');
+
+console.log('node js');
